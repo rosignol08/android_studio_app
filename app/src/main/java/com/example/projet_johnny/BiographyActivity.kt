664085d250cc61948da9a5c148308127d2bc7c67
rosignol08 <rchaffray@gmail.com>
2024-12-30 12:32:54 +0100

    padding de la soundboard

--- app/src/main/java/com/example/projet_johnny/BiographyActivity.kt
+++ app/src/main/java/com/example/projet_johnny/BiographyActivity.kt
@@ -68,18 +68,3 @@ class BiographyFragment : Fragment() {
         })
     }
 }
-
-//package com.example.projet_johnny
-
-//import android.os.Bundle
-//import androidx.appcompat.app.AppCompatActivity
-
-//class BiographyActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_biography)
-
-// Si tu veux ajouter des interactions ou charger dynamiquement des données,
-// tu peux le faire ici
-//}
-//}
